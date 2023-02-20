e6138ab3db3665c3f0f8eb72644f7f2fdc884aa7
AlexisleoMena <alexismena2690@gmail.com>
2023-02-19 23:44:22 -0300

    02-Metodos-comunes

---
+++ 04-strings-strconv/02-Metodos-comunes/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var nombre, edad, altura, casado string = "Alexis", "22", "1.74", "false"
+
+	fmt.Printf("%T, %v\n", nombre, nombre)
+
+	if s, err := strconv.Atoi(edad); err == nil {
+		fmt.Printf("%T, %v\n", s, s)
+	}
+
+	if s, err := strconv.ParseInt(edad, 10, 32); err == nil {
+		fmt.Printf("%T, %v\n", s, s)
+	}
+
+	if s, err := strconv.ParseFloat(altura, 64); err == nil {
+		fmt.Printf("%T, %v\n", s, s)
+	}
+
+	if s, err := strconv.ParseBool(casado); err == nil {
+		fmt.Printf("%T, %v\n", s, s)
+	}
+}
