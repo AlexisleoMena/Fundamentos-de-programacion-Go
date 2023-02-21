d629f5e0b90e728aacadc0605773513e2a1fc764
AlexisleoMena <alexismena2690@gmail.com>
2023-02-21 16:33:29 -0300

    09-Testing. Coverage

--- 09-Testing/testunitario/mate.go
+++ 09-Testing/testunitario/mate.go
@@ -3,3 +3,15 @@ package testunitario
 func Suma(a, b int) int {
 	return a + b
 }
+
+func GetMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+//go test -cover
+//go test -coverprofile=coverage
+//go tool cover -html=coverage
+//go tool cover -func=coverage
--- 09-Testing/testunitario/mate_test.go
+++ 09-Testing/testunitario/mate_test.go
@@ -28,3 +28,23 @@ func TestSuma(t *testing.T) {
 		}
 	}
 }
+
+func TestGetMax(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+		c int
+	}{
+		{4, 2, 4},
+		{5, 3, 5},
+		{2, 3, 3},
+	}
+
+	for _, item := range tabla {
+		max := GetMax(item.a, item.b)
+
+		if max != item.c {
+			t.Errorf("GetMax incorrecta, tiene %d se espearaba %d", max, item.c)
+		}
+	}
+}
