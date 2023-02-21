5e18ea9ac0d1ebd7dcee1c5a36db860f686d8e37
AlexisleoMena <alexismena2690@gmail.com>
2023-02-21 15:10:47 -0300

    08-Concurrencia-Canales. 01-Primer-Ejemplo. Con  concurrencia

--- 08-Concurrencia-Canales/01-Primer-Ejemplo/main.go
+++ 08-Concurrencia-Canales/01-Primer-Ejemplo/main.go
@@ -6,27 +6,34 @@ import (
 	"time"
 )
 
-func revisarServidor(servidor string) {
-	if _, err := http.Get(servidor); err != nil {
-		fmt.Println(servidor, "no esta  funcionando")
-		return
+func revisarServidor(servidor string, canal chan string) {
+	_, err := http.Get(servidor)
+	if err != nil {
+		canal <- servidor + " no esta  funcionando"
+	} else {
+		canal <- servidor + " esta  funcionando"
 	}
-	fmt.Println(servidor, "esta  funcionando")
 }
 
 func main() {
 	inicio := time.Now()
 
+	canal := make(chan string)
+
 	servidores := []string{
 		"https://oregoom.com/",
 		"https://www.udemy.com/",
-		"https://www.youtDDDube.com/",
+		"https://www.youtube.com/",
 		"https://www.facebook.com/",
 		"https://www.google.com/",
 	}
 
 	for _, servidor := range servidores {
-		revisarServidor(servidor)
+		go revisarServidor(servidor, canal)
+	}
+
+	for i := 0; i < len(servidores); i++ {
+		fmt.Println(<-canal)
 	}
 
 	fmt.Println("Tiempo de ejecución:", time.Since(inicio))
