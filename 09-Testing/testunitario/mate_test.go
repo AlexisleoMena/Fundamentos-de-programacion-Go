a76f1b664aa6d864691f743ec59a197b4a5c349c
AlexisleoMena <alexismena2690@gmail.com>
2023-02-21 16:14:11 -0300

    09-Testing.  Test de manera convencional

--- 09-Testing/testunitario/mate_test.go
+++ 09-Testing/testunitario/mate_test.go
@@ -2,10 +2,29 @@ package testunitario
 
 import "testing"
 
-func TestSuma(t *testing.T) {
-	total := Suma(5, 5)
+// func TestSuma(t *testing.T) {
+// 	total := Suma(5, 5)
 
-	if total != 10 {
-		t.Errorf("Suma incorrecta, tiene %d se espearaba %d", total, 10)
+// 	if total != 10 {
+// 		t.Errorf("Suma incorrecta, tiene %d se espearaba %d", total, 10)
+// 	}
+// }
+
+func TestSuma(t *testing.T) {
+	tabla := []struct {
+		a int
+		b int
+		c int
+	} {
+		{1, 2, 3},
+		{2, 2, 4},
+		{25, 25, 50},
+	}
+
+	for _, item := range tabla {
+		total := Suma(item.a, item.b)
+		if total != item.c {
+			t.Errorf("Suma incorrecta, tiene %d se espearaba %d", total, 10)
+		}
 	}
 }
