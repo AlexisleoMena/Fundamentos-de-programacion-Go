ac6aafa583c5f9000ce388dbcb0a97eb34f804f6
AlexisleoMena <alexismena2690@gmail.com>
2023-02-21 23:24:15 -0300

    10-Primer-servidor. Querys

--- 10-Introduccion-desarrollo-web/01-Primer-servidor/main.go
+++ 10-Introduccion-desarrollo-web/01-Primer-servidor/main.go
@@ -15,11 +15,22 @@ func NotFound(rw http.ResponseWriter, r *http.Request) {
 	http.NotFound(rw, r)
 }
 
+func Error(rw http.ResponseWriter, r *http.Request) {
+	http.Error(rw, "Este es un error", http.StatusBadRequest)
+}
+
+func Saludar(rw http.ResponseWriter, r *http.Request) {
+	nombre := r.URL.Query().Get("nombre")
+	fmt.Fprintf(rw, "Hola %s!", nombre)
+}
+
 func main() {
 
 	//Router
 	http.HandleFunc("/", Hola)
 	http.HandleFunc("/page", NotFound)
+	http.HandleFunc("/error", Error)
+	http.HandleFunc("/saludar", Saludar)
 
 	//Crear servidor
 	fmt.Println("El servidor esta corriendo en el puerto 3001")
