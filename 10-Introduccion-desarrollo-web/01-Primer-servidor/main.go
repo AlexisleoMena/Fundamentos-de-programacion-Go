63ebd885959dfe652de9bfa3bef15fee6cdacb41
AlexisleoMena <alexismena2690@gmail.com>
2023-02-21 23:34:01 -0300

    10-Primer-servidor. Mux

--- 10-Introduccion-desarrollo-web/01-Primer-servidor/main.go
+++ 10-Introduccion-desarrollo-web/01-Primer-servidor/main.go
@@ -26,15 +26,27 @@ func Saludar(rw http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	//Mux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Hola)
+	mux.HandleFunc("/page", NotFound)
+	mux.HandleFunc("/error", Error)
+	mux.HandleFunc("/saludar", Saludar)
+
 	//Router
-	http.HandleFunc("/", Hola)
-	http.HandleFunc("/page", NotFound)
-	http.HandleFunc("/error", Error)
-	http.HandleFunc("/saludar", Saludar)
+	// http.HandleFunc("/", Hola)
+	// http.HandleFunc("/page", NotFound)
+	// http.HandleFunc("/error", Error)
+	// http.HandleFunc("/saludar", Saludar)
 
 	//Crear servidor
+	server := &http.Server{
+		Addr:    "localhost:3001",
+		Handler: mux,
+	}
 	fmt.Println("El servidor esta corriendo en el puerto 3001")
 	fmt.Println("http://localhost:3001")
-	log.Fatal(http.ListenAndServe("localhost:3001", nil))
+	// log.Fatal(http.ListenAndServe("localhost:3001", nil))
+	log.Fatal(server.ListenAndServe())
 
 }
