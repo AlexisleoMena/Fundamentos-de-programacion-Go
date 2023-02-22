8ac24a75ca4d4506327b780af44ba60ef697bf6e
AlexisleoMena <alexismena2690@gmail.com>
2023-02-21 23:15:55 -0300

    10-Primer-servidor. Page Not Found

--- 10-Introduccion-desarrollo-web/01-Primer-servidor/main.go
+++ 10-Introduccion-desarrollo-web/01-Primer-servidor/main.go
@@ -6,7 +6,22 @@ import (
 	"net/http"
 )
 
+// Handlers
+func Hola(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Hola mundo!")
+}
+
+func NotFound(rw http.ResponseWriter, r *http.Request) {
+	http.NotFound(rw, r)
+}
+
 func main() {
+
+	//Router
+	http.HandleFunc("/", Hola)
+	http.HandleFunc("/page", NotFound)
+
+	//Crear servidor
 	fmt.Println("El servidor esta corriendo en el puerto 3001")
 	fmt.Println("http://localhost:3001")
 	log.Fatal(http.ListenAndServe("localhost:3001", nil))
